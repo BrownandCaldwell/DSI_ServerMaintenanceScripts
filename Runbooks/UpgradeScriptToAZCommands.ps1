f29df6b8d56dd09d2eecdccae0f1144a21838b62
csomerlot <csomerlot@brwncald.com>
2021-03-19 19:33:47 -0400

    Added Brian's Runbook scripts

---
+++ Runbooks/UpgradeScriptToAZCommands.ps1
@@ -0,0 +1,16 @@
+﻿Import-Module Az.Tools.Migration
+# Generate an upgrade plan for all the scripts and module files in the specified folder and save it to a variable.
+
+#Change DirectoryPath to location of Script
+$DirectoryPath = 'C:\Users\bmauri\OneDrive - Brown and Caldwell\Not Shared\Projects\Azure\Update Management\Scripts\New'
+New-AzUpgradeModulePlan -FromAzureRmVersion 6.13.1 -ToAzVersion 4.6.1 -DirectoryPath $DirectoryPath -OutVariable Plan
+
+# Filter plan results to only warnings and errors
+$Plan | Where-Object PlanResult -ne ReadyToUpgrade | Format-List
+
+
+# Execute the automatic upgrade plan and save the results to a variable.
+Invoke-AzUpgradeModulePlan -Plan $Plan -FileEditMode SaveChangesToNewFiles -OutVariable Results
+
+# Filter results to show only errors
+$Results | Where-Object UpgradeResult -ne UpgradeCompleted | Format-List
